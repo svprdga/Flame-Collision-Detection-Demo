02249dce01e3ea81343fc547113f67ae16dde910
svprdga <svprdga@reprom.io>
2021-12-16 19:28:02 +0100

    Detect collisions

--- lib/player.dart
+++ lib/player.dart
@@ -19,6 +19,12 @@ class Player extends PositionComponent with HasHitboxes, Collidable {
   late final Paint _paint;
 
   Vector2 _movementVector = Vector2.zero();
+  Vector2 _oldPosition = Vector2.zero();
+
+  final List<Component> _collidingLeft = [];
+  final List<Component> _collidingTop = [];
+  final List<Component> _collidingRight = [];
+  final List<Component> _collidingBot = [];
 
   // ***************************** CONSTRUCTORS **************************** //
 
@@ -38,8 +44,10 @@ class Player extends PositionComponent with HasHitboxes, Collidable {
 
   @override
   void update(double delta) {
+    _oldPosition = position.clone();
     super.update(delta);
-    position += _movementVector * delta;
+    // position += _movementVector * delta;
+    applyMovement(delta);
 
     // Never let the player go outside the screen
     if (position.x < 0.0) position.x = 0.0;
@@ -60,19 +68,82 @@ class Player extends PositionComponent with HasHitboxes, Collidable {
         _acceleration;
   }
 
+  void applyMovement(double delta) {
+    double x = _movementVector.x;
+    double y = _movementVector.y;
+
+    if ((x < 0.0 && _collidingLeft.isNotEmpty) ||
+        (x > 0.0 && _collidingRight.isNotEmpty)) {
+      x = 0.0;
+    }
+
+    if ((y > 0.0 && _collidingBot.isNotEmpty) ||
+        (y < 0.0 && _collidingTop.isNotEmpty)) {
+      y = 0.0;
+    }
+
+    position += Vector2(x, y) * delta;
+  }
+
   @override
   void onCollision(Set<Vector2> points, Collidable other) {
-    if (other is ScreenCollidable) {
-      // debugPrint('Collision with the screen');
+    double? x;
+    double? y;
+
+    // Detect left collisions
+    final otherRight = other.position.x + other.size.x;
+    if (position.x <= otherRight && _oldPosition.x > otherRight) {
+      x = otherRight;
+      _collidingLeft.add(other);
+    }
+
+    // Detect top collisions
+    final otherBot = other.position.y + other.size.y;
+    if (position.y <= otherBot && _oldPosition.y > otherBot) {
+      y = otherBot;
+      _collidingTop.add(other);
+    }
+
+    // Detect right collisions
+    if (_right() >= other.position.x && _oldRight() < other.position.x) {
+      x = other.position.x - size.x;
+      _collidingRight.add(other);
+    }
+
+    // Detect bottom collisions
+    if (_bot() >= other.position.y && _oldBot() < other.position.y) {
+      y = other.position.y - size.y;
+      _collidingBot.add(other);
+    }
+
+    // Compensate position
+    if (x != null) {
+      position.x = x;
+    }
+    if (y != null) {
+      position.y = y;
     }
   }
 
   @override
   void onCollisionEnd(Collidable other) {
-    if (other is ScreenCollidable) {}
+    for (final list in [
+      _collidingLeft,
+      _collidingTop,
+      _collidingRight,
+      _collidingBot,
+    ]) {
+      if (list.contains(other)) {
+        list.remove(other);
+      }
+    }
   }
 
   double _right() => position.x + size.x;
 
+  double _oldRight() => _oldPosition.x + size.x;
+
   double _bot() => position.y + size.y;
+
+  double _oldBot() => _oldPosition.y + size.y;
 }
